8e9810bcab22deda6418daa5b6a664f126b9dfe9
Mojib Mohammad <mmohammad3@outlook.com>
2022-10-31 20:55:20 -0700

    day1 of js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let score;
+
+score = 50;
+console.log('score is:', score);
+score = 60;
+console.log('score is:', score);
+
+let name = 'John';
+let age = 21;
+
+// Camal notation (case)
+const interestRate = 2.5;
+
+const InterestRate = 4.5;
+
+// let name = 'Mojib' // error
+
+age = 48;
+// Cannot reassign constant variable: (TypeError)
+// interestRate = 3.5
+
+// Logging age to the console
+/* 
+    first line commnent 
+    another line
+    yet another line
+
+
+*/
+console.log('age is:', age);
+console.log(name);
+console.log(interestRate);
